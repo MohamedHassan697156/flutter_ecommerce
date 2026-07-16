910dc83976c3d2332c2ae4ac35b5a9dc70891486
MohamedHassan697156 <107966291+MohamedHassan697156@users.noreply.github.com>
2026-07-16 21:37:21 +0300

    Add lib/core/constants/app_strings.dart

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,20 @@
+class AppStrings {
+  AppStrings._();
+
+  static const String appName          = 'ShopFlow';
+  static const String home             = 'Home';
+  static const String cart             = 'Cart';
+  static const String wishlist         = 'Wishlist';
+  static const String profile          = 'Profile';
+  static const String search           = 'Search products...';
+  static const String addToCart        = 'Add to Cart';
+  static const String buyNow           = 'Buy Now';
+  static const String allCategories    = 'All Categories';
+  static const String featuredProducts = 'Featured Products';
+  static const String newArrivals      = 'New Arrivals';
+  static const String emptyCart        = 'Your cart is empty';
+  static const String total            = 'Total';
+  static const String checkout         = 'Checkout';
+  static const String removeFromCart   = 'Remove';
+  static const String addToWishlist    = 'Add to Wishlist';
+}
