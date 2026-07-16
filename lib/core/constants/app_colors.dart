d2c77b6da1098f4ff5c130c042666fc1adde9584
MohamedHassan697156 <107966291+MohamedHassan697156@users.noreply.github.com>
2026-07-16 21:37:20 +0300

    Add lib/core/constants/app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary      = Color(0xFF4F8EF7);
+  static const Color primaryDark  = Color(0xFF7C5CFC);
+  static const Color secondary    = Color(0xFF22C55E);
+
+  static const Color bgDark       = Color(0xFF09090F);
+  static const Color surfaceDark  = Color(0xFF111119);
+  static const Color cardDark     = Color(0xFF16161F);
+  static const Color borderDark   = Color(0xFF1E1E2E);
+
+  static const Color bgLight      = Color(0xFFF8F9FA);
+  static const Color surfaceLight = Color(0xFFFFFFFF);
+  static const Color cardLight    = Color(0xFFFFFFFF);
+  static const Color borderLight  = Color(0xFFE5E7EB);
+
+  static const Color textDark     = Color(0xFFE8E8F0);
+  static const Color textMuted    = Color(0xFF6B6B85);
+  static const Color textLight    = Color(0xFF1A1A2E);
+
+  static const Color error        = Color(0xFFEF4444);
+  static const Color warning      = Color(0xFFF59E0B);
+  static const Color success      = Color(0xFF22C55E);
+}
